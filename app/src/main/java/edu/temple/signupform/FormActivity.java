1459759f0f458e25608ba0441f5abbb1be0d5594
Jackychulo <jackyc1440gmail.com>
2021-02-11 11:51:58 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -0,0 +1,45 @@
+package edu.temple.signupform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText name = findViewById(R.id.editText1);
+        EditText pass = findViewById(R.id.editText2);
+        TextView wuse = findViewById(R.id.inv1);
+        TextView wpas = findViewById(R.id.inv2);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(name.getText().toString().equals("admin") && pass.getText().toString().equals("admin")){
+                    Toast.makeText(FormActivity.this,
+                            "WELCOME, "+name.getText().toString(),
+                            Toast.LENGTH_SHORT).show();
+                    wuse.setVisibility(View.INVISIBLE);
+                    wpas.setVisibility(View.INVISIBLE);
+                }else{
+                    wuse.setText("WRONG USERNAME");
+                    wpas.setText("WRONG PASSWORD");
+                    wuse.setVisibility(View.VISIBLE);
+                    wpas.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+}
